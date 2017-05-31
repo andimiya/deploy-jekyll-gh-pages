c1e82f064194371821568ee26654ad7ac6befadc
Andrea <takamiya.andrea@gmail.com>
2017-05-31 13:23:47 -1000

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -5,8 +5,10 @@ git clone -b gh-pages `git config remote.origin.url` _site
 jekyll build
 cd _site
 
-touch CNAME // if you don't have a custom domain, remove this line
-echo "blog.sudokrew.com" >> CNAME  // if you don't have a custome domain, remove this line. If you do, add your custom domain here
+# if you don't have a custom domain, remove these 2 CNAME lines completely
+touch CNAME 
+# add your custom domain here
+echo "blog.sudokrew.com" >> CNAME  
 
 git add .
 git commit -m "Scripted build to gh-pages"

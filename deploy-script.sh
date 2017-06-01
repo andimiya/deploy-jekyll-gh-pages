2aaf8d84507e053e094096564d5d965ea952e938
Andrea <takamiya.andrea@gmail.com>
2017-05-31 15:11:12 -1000

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf _site
+
+# if you're using Github Pages as andimiya.github.io, replace 'gh-pages' below with 'master'
 git clone -b gh-pages `git config remote.origin.url` _site
 jekyll build
 cd _site
